ef75714620892460d9657805a59da72f9d2bd9de
rafaelscdev <128648524+rafaelscdev@users.noreply.github.com>
2024-02-16 12:20:30 -0300

    Add file script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let numero = 53
+
+function parImpar(){
+    if (numero % 2 ==0){
+        console.log(`${numero} é um número par!`)
+    }
+    else{
+    console.log(`${numero} é um número impar!`)
+    }
+}
+
+parImpar()
